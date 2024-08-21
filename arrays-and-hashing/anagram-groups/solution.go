9d57141ae2f1b5effd2ebd5a882567f1f87ffc74
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-21 23:25:35 +0300

    solution(anagram groups): solve the problem by creating a unique key from the sorted characters

--- arrays-and-hashing/anagram-groups/solution.go
+++ arrays-and-hashing/anagram-groups/solution.go
@@ -1,5 +1,39 @@
 package anagramgroups
 
+import "slices"
+
 func GroupAnagrams(strs []string) [][]string {
-	return nil
+	if len(strs) < 2 {
+		return [][]string{strs}
+	}
+
+	storage := make(map[string][]string, 0)
+
+	for _, val := range strs {
+		key := makeUniqueKey(val)
+
+		group := storage[key]
+		if len(group) == 0 {
+			storage[key] = []string{val}
+		} else {
+			storage[key] = append(group, val)
+		}
+	}
+
+	idx := 0
+	groups := make([][]string, len(storage))
+	for _, group := range storage {
+		groups[idx] = group
+		idx++
+	}
+
+	return groups
+}
+
+func makeUniqueKey(s string) string {
+	keyBytes := []byte(s)
+
+	slices.Sort(keyBytes)
+
+	return string(keyBytes)
 }
