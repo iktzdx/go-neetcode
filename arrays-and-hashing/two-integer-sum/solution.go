9b4d05c996248c5a48ad2f385ddfb311aa218294
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-21 17:02:58 +0300

    solution(two integer sum): fill the map while traversing a slice

--- arrays-and-hashing/two-integer-sum/solution.go
+++ arrays-and-hashing/two-integer-sum/solution.go
@@ -2,21 +2,19 @@ package twosum
 
 func TwoSum(nums []int, target int) []int {
 	idxs := make([]int, 2)
-
 	storage := make(map[int]int, len(nums))
-	for i, v := range nums {
-		// If there are 2 equal elements in the slice,
-		// save the index of the last one.
-		storage[v] = i
-	}
 
-	for i := range nums {
+	for i, v := range nums {
 		sub := target - nums[i]
-		if idx, exists := storage[sub]; exists && idx != i {
-			idxs[0], idxs[1] = i, idx
+
+		idx, exists := storage[sub]
+		if exists {
+			idxs[0], idxs[1] = idx, i
 
 			break
 		}
+
+		storage[v] = i
 	}
 
 	return idxs
