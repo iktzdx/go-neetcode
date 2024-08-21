5221e117a67d969e0b1af4c0b163ce6447e57e20
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-22 00:54:39 +0300

    test(anagram groups): simplify group comparison

--- arrays-and-hashing/anagram-groups/solution_test.go
+++ arrays-and-hashing/anagram-groups/solution_test.go
@@ -1,12 +1,12 @@
 package anagramgroups_test
 
 import (
+	"fmt"
+	"reflect"
 	"slices"
-	"sort"
 	"testing"
 
 	anagramgroups "github.com/iktzdx/go-neetcode/arrays-and-hashing/anagram-groups"
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_GroupAnagrams(t *testing.T) {
@@ -39,41 +39,28 @@ func Test_GroupAnagrams(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := anagramgroups.GroupAnagrams(test.strs)
-			if !compareGroups(t, test.want, got) {
+			if !compareGroups(test.want, got) {
 				t.Fatalf("GroupAnagrams(%v): expected = %v, got = %v", test.strs, test.want, got)
 			}
 		})
 	}
 }
 
-func compareGroups(t *testing.T, want, got [][]string) bool {
-	sort.Slice(want, func(i, j int) bool {
-		if len(want[i]) == len(want[j]) {
-			slices.Sort(want[i])
-			slices.Sort(want[j])
-
-			return want[i][0] < want[j][0]
-		}
-
-		return len(want[i]) < len(want[j])
-	})
-
-	sort.Slice(got, func(i, j int) bool {
-		if len(got[i]) == len(got[j]) {
-			slices.Sort(got[i])
-			slices.Sort(got[j])
-
-			return got[i][0] < got[j][0]
-		}
-
-		return len(got[i]) < len(got[j])
-	})
-
-	for i, group := range got {
-		if !assert.ElementsMatch(t, want[i], group) {
-			return false
-		}
+func compareGroups(want, got [][]string) bool {
+	if len(want) != len(got) {
+		return false
 	}
 
-	return true
+	return reflect.DeepEqual(sliceToMap(want), sliceToMap(got))
+}
+
+func sliceToMap(ss [][]string) map[string]int {
+	m := make(map[string]int, 0)
+
+	for _, s := range ss {
+		slices.Sort(s)
+		m[fmt.Sprint(s)]++
+	}
+
+	return m
 }
