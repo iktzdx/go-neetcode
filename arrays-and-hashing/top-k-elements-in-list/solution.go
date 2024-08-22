c565f30ab97d45146ffc1fc96f4864b8b28013f6
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-22 22:31:51 +0300

    solution(top k elements in list): solve the problem by sorting the numbers by their frequency - O(n log n)

--- arrays-and-hashing/top-k-elements-in-list/solution.go
+++ arrays-and-hashing/top-k-elements-in-list/solution.go
@@ -1,5 +1,25 @@
 package topkelementsinlist
 
+import "sort"
+
 func TopKFrequent(nums []int, k int) []int {
-	return nil
+	if len(nums) < 2 {
+		return nums
+	}
+
+	freqs := make(map[int]int, 0)
+	for _, n := range nums {
+		freqs[n]++
+	}
+
+	keys := make([]int, 0)
+	for k := range freqs {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return freqs[keys[i]] > freqs[keys[j]]
+	})
+
+	return keys[:k]
 }
