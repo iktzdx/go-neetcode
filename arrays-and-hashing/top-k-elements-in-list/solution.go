7d9ebb22827c013e7ee53cc290e412719603a062
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-22 23:31:35 +0300

    solution(top k elements in list): solve the problem using heap - O(k log n)

--- arrays-and-hashing/top-k-elements-in-list/solution.go
+++ arrays-and-hashing/top-k-elements-in-list/solution.go
@@ -1,25 +1,68 @@
 package topkelementsinlist
 
-import "sort"
+import (
+	"container/heap"
+)
+
+type (
+	Item struct {
+		key   int
+		value int
+	}
+
+	MinHeap []*Item
+)
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i].value < h[j].value
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(*Item))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+
+	return x
+}
 
 func TopKFrequent(nums []int, k int) []int {
 	if len(nums) < 2 {
 		return nums
 	}
 
-	freqs := make(map[int]int, 0)
+	freqs := make(map[int]int, k)
 	for _, n := range nums {
 		freqs[n]++
 	}
 
-	keys := make([]int, 0)
-	for k := range freqs {
-		keys = append(keys, k)
+	h := &MinHeap{}
+	heap.Init(h)
+
+	for k, v := range freqs {
+		heap.Push(h, &Item{key: k, value: v})
 	}
 
-	sort.SliceStable(keys, func(i, j int) bool {
-		return freqs[keys[i]] > freqs[keys[j]]
-	})
+	for h.Len() > k {
+		heap.Pop(h)
+	}
 
-	return keys[:k]
+	res := make([]int, 0)
+	for _, el := range *h {
+		res = append(res, el.key)
+	}
+
+	return res
 }
