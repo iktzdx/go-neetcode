2da9b8387ebe8bf8e13fcff9ff8296b5bb0c90e1
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-23 23:40:41 +0300

    solution(encode and decode strings): resolve the problem - O(n)

--- arrays-and-hashing/encode-and-decode-strings/solution.go
+++ arrays-and-hashing/encode-and-decode-strings/solution.go
@@ -1,9 +1,43 @@
 package encodeanddecodestrings
 
+import (
+	"fmt"
+	"strconv"
+)
+
+const delimiter rune = '#'
+
 func Encode(strs []string) string {
-	return ""
+	var res string
+
+	for _, str := range strs {
+		res += fmt.Sprintf("%d%s%s", len(str), string(delimiter), str)
+	}
+
+	return res
 }
 
-func Decode(s string) []string {
-	return []string{}
+func Decode(str string) []string {
+	res := make([]string, 0)
+
+	start := 0
+	rs := []rune(str)
+
+	for start < len(rs) {
+		end := start
+		for rs[end] != rune(delimiter) {
+			end++
+		}
+
+		length, err := strconv.Atoi(string(rs[start:end]))
+		if err != nil {
+			return res
+		}
+
+		res = append(res, string(rs[end+1:end+1+length]))
+
+		start = end + 1 + length
+	}
+
+	return res
 }
