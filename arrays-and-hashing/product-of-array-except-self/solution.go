2006d829d027dab4be14c8584cca1719aa4ea1b2
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-24 19:08:27 +0300

    solution(product of array except self): change algo to run the solution in O(n) time

--- arrays-and-hashing/product-of-array-except-self/solution.go
+++ arrays-and-hashing/product-of-array-except-self/solution.go
@@ -1,25 +1,45 @@
 package productofarrayexceptself
 
 func ProductExceptSelf(nums []int) []int {
-	answer := make([]int, len(nums))
+	length := len(nums)
 
-	for i := range answer {
-		answer[i] = productOfArray(nums, i)
-	}
+	pre := make([]int, length)
+	post := make([]int, length)
+	out := make([]int, length)
 
-	return answer
-}
-
-func productOfArray(nums []int, exclude int) int {
-	num := 1
-
-	for i, n := range nums {
-		if i == exclude {
+	for i := 0; i < length; i++ {
+		if i == 0 {
+			pre[i] = 1 * nums[i]
 			continue
 		}
 
-		num *= n
+		pre[i] = pre[i-1] * nums[i]
 	}
 
-	return num
+	for i := length - 1; i >= 0; i-- {
+		if i == length-1 {
+			post[i] = 1 * nums[i]
+			continue
+		}
+
+		post[i] = post[i+1] * nums[i]
+	}
+
+	for i := 0; i < length; i++ {
+		defaultMultiplier := 1
+
+		if i == 0 {
+			out[i] = defaultMultiplier * post[i+1]
+			continue
+		}
+
+		if i == length-1 {
+			out[i] = pre[i-1] * defaultMultiplier
+			continue
+		}
+
+		out[i] = pre[i-1] * post[i+1]
+	}
+
+	return out
 }
