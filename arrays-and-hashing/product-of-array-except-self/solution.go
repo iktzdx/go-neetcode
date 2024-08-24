12835b363b4e46dcaad8640e0d8b6a57a9e3bca7
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-24 19:34:52 +0300

    solution(product of array except self): get rid of the length variable to reduce memory usage

--- arrays-and-hashing/product-of-array-except-self/solution.go
+++ arrays-and-hashing/product-of-array-except-self/solution.go
@@ -1,18 +1,16 @@
 package productofarrayexceptself
 
 func ProductExceptSelf(nums []int) []int {
-	length := len(nums)
-
-	out := make([]int, length)
+	out := make([]int, len(nums))
 
 	prefix := 1
-	for i := 0; i < length; i++ {
+	for i := 0; i < len(nums); i++ {
 		out[i] = prefix
 		prefix *= nums[i]
 	}
 
 	postfix := 1
-	for i := length - 1; i >= 0; i-- {
+	for i := len(nums) - 1; i >= 0; i-- {
 		out[i] *= postfix
 		postfix *= nums[i]
 	}
