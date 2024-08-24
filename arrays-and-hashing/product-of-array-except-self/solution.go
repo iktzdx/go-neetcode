be56524c889ed8972343b3965a784e6f11c8aaa8
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-24 19:31:10 +0300

    solution(product of array except self): remove redundant slices to achieve O(1) space complexity

--- arrays-and-hashing/product-of-array-except-self/solution.go
+++ arrays-and-hashing/product-of-array-except-self/solution.go
@@ -3,42 +3,18 @@ package productofarrayexceptself
 func ProductExceptSelf(nums []int) []int {
 	length := len(nums)
 
-	pre := make([]int, length)
-	post := make([]int, length)
 	out := make([]int, length)
 
+	prefix := 1
 	for i := 0; i < length; i++ {
-		if i == 0 {
-			pre[i] = 1 * nums[i]
-			continue
-		}
-
-		pre[i] = pre[i-1] * nums[i]
+		out[i] = prefix
+		prefix *= nums[i]
 	}
 
+	postfix := 1
 	for i := length - 1; i >= 0; i-- {
-		if i == length-1 {
-			post[i] = 1 * nums[i]
-			continue
-		}
-
-		post[i] = post[i+1] * nums[i]
-	}
-
-	for i := 0; i < length; i++ {
-		defaultMultiplier := 1
-
-		if i == 0 {
-			out[i] = defaultMultiplier * post[i+1]
-			continue
-		}
-
-		if i == length-1 {
-			out[i] = pre[i-1] * defaultMultiplier
-			continue
-		}
-
-		out[i] = pre[i-1] * post[i+1]
+		out[i] *= postfix
+		postfix *= nums[i]
 	}
 
 	return out
