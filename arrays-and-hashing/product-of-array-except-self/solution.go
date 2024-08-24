28cb082f357b63743410214492c3e767c6ba511a
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-24 18:08:05 +0300

    solution(product of array except self): improve the previous solution, but still exceeded the time limit

--- arrays-and-hashing/product-of-array-except-self/solution.go
+++ arrays-and-hashing/product-of-array-except-self/solution.go
@@ -2,25 +2,24 @@ package productofarrayexceptself
 
 func ProductExceptSelf(nums []int) []int {
 	answer := make([]int, len(nums))
+
 	for i := range answer {
-		answer[i] = 1
-	}
-
-	i, j := 0, 0
-	for i < len(nums) && j < len(nums) {
-		if j != i {
-			answer[i] *= nums[j]
-		}
-
-		if j == len(nums)-1 {
-			i++
-			j = 0
-
-			continue
-		}
-
-		j++
+		answer[i] = productOfArray(nums, i)
 	}
 
 	return answer
+}
+
+func productOfArray(nums []int, exclude int) int {
+	num := 1
+
+	for i, n := range nums {
+		if i == exclude {
+			continue
+		}
+
+		num *= n
+	}
+
+	return num
 }
