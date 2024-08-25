57019f3e594de90ca68dc92ffc90bf8bd9057fc5
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-25 17:59:48 +0300

    solution(valid sudoku): check rows and columns separately

--- arrays-and-hashing/valid-sudoku/solution.go
+++ arrays-and-hashing/valid-sudoku/solution.go
@@ -1,5 +1,42 @@
 package validsudoku
 
+const (
+	boardSideLength int  = 9
+	emptyCell       byte = '.'
+)
+
 func IsValidSudoku(board [][]byte) bool {
 	return false
+}
+
+func CheckRow(row []byte) bool {
+	rowMap := make(map[byte]struct{}, boardSideLength)
+
+	for _, rowVal := range row {
+		if _, exists := rowMap[rowVal]; exists && rowVal != emptyCell {
+			return false
+		}
+
+		rowMap[rowVal] = struct{}{}
+	}
+
+	return true
+}
+
+func CheckColumn(board [][]byte) bool {
+	colMap := make(map[byte]struct{}, boardSideLength)
+
+	for colIdx := 0; colIdx < boardSideLength; colIdx++ {
+		for rowIdx := 0; rowIdx < boardSideLength; rowIdx++ {
+			colVal := board[rowIdx][colIdx]
+			if _, exists := colMap[colVal]; exists && colVal != emptyCell {
+				return false
+			}
+
+			colMap[colVal] = struct{}{}
+		}
+		clear(colMap)
+	}
+
+	return true
 }
