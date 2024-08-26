7f3ce6f69fe10379ab927b2f545f7f05b7c34dad
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-26 17:11:34 +0300

    solution(longest consecutive sequence): solve the problem using sorting - O(n log n)

--- arrays-and-hashing/longest-consecutive-sequence/solution.go
+++ arrays-and-hashing/longest-consecutive-sequence/solution.go
@@ -1,6 +1,20 @@
 package longestconsecutivesequence
 
-func LongestConsecutive(nums []int) int {
-	return -1
-}
+import "slices"
 
+func LongestConsecutive(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	slices.Sort(nums)
+
+	counter := 1
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1]-1 {
+			counter++
+		}
+	}
+
+	return counter
+}
