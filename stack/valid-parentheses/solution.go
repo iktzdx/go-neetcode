b4356176715417d9d9828a4cd1141c9a777d7833
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-27 22:28:57 +0300

    solution(valid parentheses): solve the problem using LIFO - O(n)

--- stack/valid-parentheses/solution.go
+++ stack/valid-parentheses/solution.go
@@ -1,5 +1,27 @@
 package validparentheses
 
 func IsValid(s string) bool {
-	return false
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	brackets := map[string]string{"}": "{", ")": "(", "]": "["}
+
+	if _, found := brackets[string(s[0])]; found {
+		return false
+	}
+
+	stack := []string{}
+
+	for _, val := range s {
+		match, found := brackets[string(val)]
+		if found && match == stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+
+		stack = append(stack, string(val))
+	}
+
+	return len(stack) == 0
 }
