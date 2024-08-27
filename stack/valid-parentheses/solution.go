ff99bc49da5b1906739f2da3bb3149a93699e300
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-27 23:41:03 +0300

    solution(valid parentheses): implement a basic stack data structure

--- stack/valid-parentheses/solution.go
+++ stack/valid-parentheses/solution.go
@@ -1,41 +1,53 @@
 package validparentheses
 
+type stack []rune
+
+func (s *stack) len() int {
+	return len(*s)
+}
+
+func (s *stack) push(val rune) {
+	*s = append(*s, val)
+}
+
+func (s *stack) pop() rune {
+	if s.isEmpty() {
+		return -1
+	}
+
+	last := (*s)[s.len()-1]
+	*s = (*s)[:s.len()-1]
+
+	return last
+}
+
+func (s *stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
 func IsValid(s string) bool {
 	if len(s)%2 != 0 {
 		return false
 	}
 
-	stack := []rune{}
+	stack := &stack{}
+	pairs := map[rune]rune{']': '[', ')': '(', '}': '{'}
 
 	for _, val := range s {
-		switch val {
-		case '[', '(', '{':
-			stack = append(stack, val)
-		case ']', ')', '}':
-			if len(stack) == 0 {
-				return false
-			}
+		match, found := pairs[val]
+		if found && stack.isEmpty() {
+			return false
+		}
 
-			if !isMatching(stack[len(stack)-1], val) {
-				return false
-			}
+		if !found {
+			stack.push(val)
+			continue
+		}
 
-			stack = stack[:len(stack)-1]
+		if last := stack.pop(); last != match {
+			return false
 		}
 	}
 
-	return len(stack) == 0
-}
-
-func isMatching(o, c rune) bool {
-	switch o {
-	case '[':
-		return c == ']'
-	case '{':
-		return c == '}'
-	case '(':
-		return c == ')'
-	}
-
-	return false
+	return stack.isEmpty()
 }
