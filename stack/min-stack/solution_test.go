1b4497b7e22bf1d066c5e388553c23c591daa481
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-28 17:01:47 +0300

    test(min stack): change test to have identical minimum values

--- stack/min-stack/solution_test.go
+++ stack/min-stack/solution_test.go
@@ -14,16 +14,17 @@ func Test_MinStack(t *testing.T) {
 
 	minStack.Push(1)
 	minStack.Push(0)
+	minStack.Push(-1)
 	minStack.Push(2)
 	minStack.Push(-1)
 
-	require.Equal(t, minStack.Len(), 4, "stack size after several pushes")
+	require.Equal(t, minStack.Len(), 5, "stack size after several pushes")
 	require.Equal(t, minStack.GetMin(), -1, "min stack element")
+	require.Equal(t, minStack.Top(), -1, "top stack element")
 
 	minStack.Pop()
 
-	require.Equal(t, minStack.Len(), 3, "stack size after a single pop")
+	require.Equal(t, minStack.Len(), 4, "stack size after a single pop")
 	require.Equal(t, minStack.Top(), 2, "new top stack element")
-
-	require.Equal(t, minStack.GetMin(), 0, "new min stack element")
+	require.Equal(t, minStack.GetMin(), -1, "new min stack element")
 }
