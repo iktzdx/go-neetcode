3ef0999a2eb8ec66dc4facd6f1d3f4a675bf827a
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-29 21:34:04 +0300

    solution(evaluate reverse polish notation): solve the problem using slice as a basic stack

---
+++ stack/evaluate-reverse-polish-notation/solution.go
@@ -0,0 +1,43 @@
+package evaluatereversepolishnotation
+
+import "strconv"
+
+func EvalRPN(tokens []string) int {
+	var stack []int
+
+	operators := map[string]bool{"+": true, "-": true, "*": true, "/": true}
+
+	for i := 0; i < len(tokens); i++ {
+		for !operators[tokens[i]] {
+			val, _ := strconv.Atoi(tokens[i])
+			stack = append(stack, val)
+
+			i++
+		}
+
+		a := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		b := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		stack = append(stack, calc(b, a, tokens[i]))
+	}
+
+	return stack[len(stack)-1]
+}
+
+func calc(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	}
+
+	return -1 // for compiler
+}
