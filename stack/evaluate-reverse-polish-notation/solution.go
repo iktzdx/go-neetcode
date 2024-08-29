9d46edcf95f0cb54531df49cdef20c175952aad7
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-29 21:39:21 +0300

    solution(evaluate reverse polish notation): handle case with a single token

--- stack/evaluate-reverse-polish-notation/solution.go
+++ stack/evaluate-reverse-polish-notation/solution.go
@@ -3,8 +3,13 @@ package evaluatereversepolishnotation
 import "strconv"
 
 func EvalRPN(tokens []string) int {
-	var stack []int
+	if len(tokens) == 1 {
+		val, _ := strconv.Atoi(tokens[0])
 
+		return val
+	}
+
+	stack := []int{}
 	operators := map[string]bool{"+": true, "-": true, "*": true, "/": true}
 
 	for i := 0; i < len(tokens); i++ {
