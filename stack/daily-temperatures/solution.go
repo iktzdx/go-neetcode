ae34a6ac7261f6c9b29408736dbbbb3d516b2e4b
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-31 18:53:30 +0300

    solution(daily temperature): push only indices onto the stack

--- stack/daily-temperatures/solution.go
+++ stack/daily-temperatures/solution.go
@@ -1,39 +1,21 @@
 package dailytemperatures
 
-type stack [][2]int
-
 func DailyTemperatures(temperatures []int) []int {
 	if len(temperatures) < 2 {
 		return []int{0}
 	}
 
-	var s stack
+	stack := make([]int, 0)
 	answer := make([]int, len(temperatures))
 
 	for i, temp := range temperatures {
-		for !s.isEmpty() && s.top()[0] < temp {
-			answer[s.top()[1]] = i - s.top()[1]
-			s.pop()
+		for len(stack) > 0 && temperatures[stack[len(stack)-1]] < temp {
+			answer[stack[len(stack)-1]] = i - stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
 		}
 
-		s.push([2]int{temp, i})
+		stack = append(stack, i)
 	}
 
 	return answer
-}
-
-func (s *stack) top() [2]int {
-	return (*s)[len(*s)-1]
-}
-
-func (s *stack) pop() {
-	(*s) = (*s)[:len(*s)-1]
-}
-
-func (s *stack) push(v [2]int) {
-	(*s) = append(*s, v)
-}
-
-func (s *stack) isEmpty() bool {
-	return len(*s) == 0
 }
