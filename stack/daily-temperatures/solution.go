b991857b235d4ea8efc826f18cea3c9e8af9dd42
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-08-31 18:44:21 +0300

    solution(daily temperature): improve the previous solution - use a monotonic stack data structure

--- stack/daily-temperatures/solution.go
+++ stack/daily-temperatures/solution.go
@@ -1,20 +1,39 @@
 package dailytemperatures
 
+type stack [][2]int
+
 func DailyTemperatures(temperatures []int) []int {
 	if len(temperatures) < 2 {
 		return []int{0}
 	}
 
+	var s stack
 	answer := make([]int, len(temperatures))
 
-	for i := 0; i < len(temperatures); i++ {
-		for j := i + 1; j < len(temperatures); j++ {
-			if temperatures[j] > temperatures[i] {
-				answer[i] = j - i
-				break
-			}
+	for i, temp := range temperatures {
+		for !s.isEmpty() && s.top()[0] < temp {
+			answer[s.top()[1]] = i - s.top()[1]
+			s.pop()
 		}
+
+		s.push([2]int{temp, i})
 	}
 
 	return answer
+}
+
+func (s *stack) top() [2]int {
+	return (*s)[len(*s)-1]
+}
+
+func (s *stack) pop() {
+	(*s) = (*s)[:len(*s)-1]
+}
+
+func (s *stack) push(v [2]int) {
+	(*s) = append(*s, v)
+}
+
+func (s *stack) isEmpty() bool {
+	return len(*s) == 0
 }
