5edafe1e46be7f008aceb206fc876e71d63b58c8
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-01 23:20:53 +0300

    solution(car fleet): solve the problem
    - Sort pairs position/speed for each car.
    - Use stack to store and compare time to destination.

--- stack/car-fleet/solution.go
+++ stack/car-fleet/solution.go
@@ -1,5 +1,29 @@
 package carfleet
 
+import (
+	"sort"
+)
+
 func CarFleet(target int, position []int, speed []int) int {
-	return -1
+	var pairs [][2]int
+	for idx, val := range position {
+		pairs = append(pairs, [2]int{val, speed[idx]})
+	}
+
+	sort.SliceStable(pairs, func(i, j int) bool {
+		return pairs[i][0] < pairs[j][0]
+	})
+
+	var stack []int
+	for i := len(pairs) - 1; i >= 0; i-- {
+		t := (target - pairs[i][0]) / pairs[i][1]
+
+		if len(stack) > 0 && stack[len(stack)-1] >= t {
+			continue
+		}
+
+		stack = append(stack, t)
+	}
+
+	return len(stack)
 }
