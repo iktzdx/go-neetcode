7a8a638a6a6e554d764b565771c758cf674c0937
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-01 21:33:03 +0300

    test(car fleet): write tests first

---
+++ stack/car-fleet/solution_test.go
@@ -0,0 +1,44 @@
+package carfleet_test
+
+import (
+	"testing"
+
+	solution "github.com/iktzdx/go-neetcode/stack/car-fleet"
+)
+
+func Test_CarFleet(t *testing.T) {
+	tests := map[string]struct {
+		target   int
+		position []int
+		speed    []int
+		want     int
+	}{
+		"multiple car fleets with different arrival time": {
+			target:   12,
+			position: []int{10, 8, 0, 5, 3},
+			speed:    []int{2, 4, 1, 1, 3},
+			want:     3,
+		},
+		"only one car": {
+			target:   10,
+			position: []int{3},
+			speed:    []int{3},
+			want:     1,
+		},
+		"multiple fleets become one": {
+			target:   100,
+			position: []int{0, 2, 4},
+			speed:    []int{4, 2, 1},
+			want:     1,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := solution.CarFleet(test.target, test.position, test.speed)
+			if test.want != got {
+				t.Fatalf("CarFleet(%v, %v, %v): expected = %v, got = %v", test.target, test.position, test.speed, test.want, got)
+			}
+		})
+	}
+}
