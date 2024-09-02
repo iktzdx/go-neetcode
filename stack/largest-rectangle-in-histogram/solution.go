9b33ab60da06b3c372028251b427f2d1315e8b2c
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-02 22:49:45 +0300

    solution(largest rectangle in histogram): solve the problem using a stack data structure - O(n)

--- stack/largest-rectangle-in-histogram/solution.go
+++ stack/largest-rectangle-in-histogram/solution.go
@@ -1,5 +1,33 @@
 package largestrectangleinhistogram
 
 func LargestRectangleArea(heights []int) int {
-	return -1
+	if len(heights) < 2 {
+		return heights[0]
+	}
+
+	var maxArea int
+	var stack [][2]int // ( index:height )
+
+	for i, h := range heights {
+		start := i
+
+		for len(stack) > 0 && stack[len(stack)-1][1] > h {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			maxArea = max(maxArea, top[1]*(i-top[0]))
+			start = top[0]
+		}
+
+		stack = append(stack, [2]int{start, h})
+	}
+
+	for len(stack) > 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		maxArea = max(maxArea, top[1]*(len(heights)-top[0]))
+	}
+
+	return maxArea
 }
