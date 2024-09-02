fb7cd82b7b0e59f30ed81c655783ef77c8e8fa41
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-02 23:00:17 +0300

    solution(largest rectangle in histogram): improve runtime by storing only indices in the stack

--- stack/largest-rectangle-in-histogram/solution.go
+++ stack/largest-rectangle-in-histogram/solution.go
@@ -5,28 +5,28 @@ func LargestRectangleArea(heights []int) int {
 		return heights[0]
 	}
 
+	// Append a zero height to ensure we can process all bars in the stack.
+	heights = append(heights, 0)
+
 	var maxArea int
-	var stack [][2]int // ( index:height )
+	var stack []int // store only indices
 
 	for i, h := range heights {
-		start := i
-
-		for len(stack) > 0 && stack[len(stack)-1][1] > h {
-			top := stack[len(stack)-1]
+		// While the current height is less than the height of the bar at the index
+		// on top of the stack, pop the stack and calculate the area.
+		for len(stack) > 0 && heights[stack[len(stack)-1]] > h {
+			height := heights[stack[len(stack)-1]]
 			stack = stack[:len(stack)-1]
 
-			maxArea = max(maxArea, top[1]*(i-top[0]))
-			start = top[0]
+			width := i
+			if len(stack) > 0 {
+				width -= stack[len(stack)-1] + 1
+			}
+
+			maxArea = max(maxArea, height*width)
 		}
 
-		stack = append(stack, [2]int{start, h})
-	}
-
-	for len(stack) > 0 {
-		top := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		maxArea = max(maxArea, top[1]*(len(heights)-top[0]))
+		stack = append(stack, i)
 	}
 
 	return maxArea
