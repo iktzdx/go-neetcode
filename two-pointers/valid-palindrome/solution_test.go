aa40af9cc86e7c2c5b56b3e98bf3daa472eddd1c
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-03 20:39:00 +0300

    test(valid palindrome): write tests first

---
+++ two-pointers/valid-palindrome/solution_test.go
@@ -0,0 +1,36 @@
+package validpalindrome_test
+
+import (
+	"testing"
+
+	solution "github.com/iktzdx/go-neetcode/two-pointers/valid-palindrome"
+)
+
+func Test_IsPalindrome(t *testing.T) {
+	tests := map[string]struct {
+		input string
+		want  bool
+	}{
+		"is valid palindrome": {
+			input: "A man, a plan, a canal: Panama",
+			want:  true,
+		},
+		"is invalid palindrome": {
+			input: "race a car",
+			want:  false,
+		},
+		"empty string": {
+			input: " ",
+			want:  true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := solution.IsPalindrome(test.input)
+			if test.want != got {
+				t.Fatalf("IsPalindrome(%v): expected = %v, got = %v", test.input, test.want, got)
+			}
+		})
+	}
+}
