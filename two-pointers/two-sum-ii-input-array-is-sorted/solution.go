56934154618064132f5c301ab675a8378bd912ea
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-04 22:53:30 +0300

    solution(two sum ii input array is sorted): improve runtime of the previous solution

--- two-pointers/two-sum-ii-input-array-is-sorted/solution.go
+++ two-pointers/two-sum-ii-input-array-is-sorted/solution.go
@@ -8,12 +8,20 @@ func TwoSum(numbers []int, target int) []int {
 	}
 
 	left, right := 0, length-1
-	for numbers[left]+numbers[right] != target {
-		if numbers[left]+numbers[right] > target {
+	for left < right {
+		sum := numbers[left] + numbers[right]
+
+		if sum > target {
 			right--
-		} else {
-			left++
+			continue
 		}
+
+		if sum < target {
+			left++
+			continue
+		}
+
+		break
 	}
 
 	return []int{left + 1, right + 1}
