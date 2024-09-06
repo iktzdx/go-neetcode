0a9cb09c17a3d6abb1566f20dd109414ebdc972b
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-06 21:20:54 +0300

    solution(container with most water): solve the problem using two pointers - O(n)

--- two-pointers/container-with-most-water/solution.go
+++ two-pointers/container-with-most-water/solution.go
@@ -3,9 +3,14 @@ package containerwithmostwater
 func MaxArea(height []int) int {
 	var maxArea int
 
-	for left := 0; left < len(height)-1; left++ {
-		for right := left + 1; right < len(height); right++ {
-			maxArea = max(maxArea, min(height[left], height[right])*(right-left))
+	left, right := 0, len(height)-1
+	for left < right {
+		maxArea = max(maxArea, (right-left)*min(height[right], height[left]))
+
+		if height[right] > height[left] {
+			left++
+		} else {
+			right--
 		}
 	}
 
