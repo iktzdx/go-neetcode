64e04e7b572b864ce441f1974f84f821534ef905
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-10 21:36:18 +0300

    solution(longest repeating character replacement): solve the problem using sliding window technique - O(n)

---
+++ sliding-window/longest-repeating-character-replacement/solution.go
@@ -0,0 +1,24 @@
+package longestrepeatingcharacterreplacement
+
+func CharacterReplacement(s string, k int) int {
+	if len(s) < 2 || len(s) == k {
+		return len(s)
+	}
+
+	var mostFreq, res int
+	freqs := make(map[byte]int, 26)
+
+	for l, r := 0, 0; r < len(s); r++ {
+		freqs[s[r]]++
+
+		mostFreq = max(mostFreq, freqs[s[r]])
+		if (r-l+1)-mostFreq > k {
+			freqs[s[l]]--
+			l++
+		}
+
+		res = max(res, r-l+1)
+	}
+
+	return res
+}
