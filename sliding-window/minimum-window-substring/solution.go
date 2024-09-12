a2f5fb0565ace10ce2bceb3c4e740f0a7a6ec6af
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-12 23:36:42 +0300

    solution(minimum window with characters): fix a bug and optimize the solution

--- sliding-window/minimum-window-substring/solution.go
+++ sliding-window/minimum-window-substring/solution.go
@@ -3,50 +3,49 @@ package minimumwindowsubstring
 const maxLen = 100001
 
 func MinWindow(s string, t string) string {
-	if s == t {
+	if len(s) == 0 || len(t) == 0 || len(s) < len(t) {
+		return ""
+	} else if s == t {
 		return t
 	}
 
+	var result string
+
 	need := make(map[rune]int, len(t))
-	have := make(map[rune]int, 0)
 	for _, ch := range t {
 		need[ch]++
 	}
 
-	needCount, haveCount := len(t), 0
-	res, resLen := [2]int{0, 0}, maxLen
+	needCount := len(t)
+	start, resLen := 0, maxLen
 
-	var result string
-
-	left := 0
-	for right := 0; right < len(s); right++ {
+	left, right := 0, 0
+	for right < len(s) {
 		ch := rune(s[right])
-		have[ch]++
-
-		if count, ok := need[ch]; ok && have[ch] == count {
-			haveCount++
+		if need[ch] > 0 {
+			needCount--
 		}
 
-		for haveCount == needCount {
-			// update result
-			if (right - left + 1) < resLen {
-				res = [2]int{left, right}
-				resLen = right - left + 1
+		need[ch]--
+		right++
+
+		for needCount == 0 {
+			if right-left < resLen {
+				start = left
+				resLen = right - left
 			}
 
-			// pop from the left
 			ch := rune(s[left])
-			have[ch]--
-
-			if count, ok := need[ch]; ok && have[ch] < count {
-				haveCount--
+			if need[ch] == 0 {
+				needCount++
 			}
+			need[ch]++
 			left++
 		}
 	}
 
 	if resLen != maxLen {
-		result = s[res[0] : res[1]+1]
+		result = s[start : start+resLen]
 	}
 
 	return result
