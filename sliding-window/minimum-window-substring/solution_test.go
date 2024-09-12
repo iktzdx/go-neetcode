e165c2af4b6b5e5cfb30c1858d907b8e28938d21
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-12 22:00:09 +0300

    test(minimum window with characters): write tests first

---
+++ sliding-window/minimum-window-substring/solution_test.go
@@ -0,0 +1,50 @@
+package minimumwindowsubstring_test
+
+import (
+	"testing"
+
+	solution "github.com/iktzdx/go-neetcode/sliding-window/minimum-window-substring"
+)
+
+func Test_MinWindow(t *testing.T) {
+	tests := map[string]struct {
+		s    string
+		t    string
+		want string
+	}{
+		"upper case letters": {
+			s:    "ADOBECODEBANC",
+			t:    "ABC",
+			want: "BANC",
+		},
+		"lower case letter": {
+			s:    "ouzodyxazv",
+			t:    "xyz",
+			want: "yxaz",
+		},
+		"single character": {
+			s:    "a",
+			t:    "a",
+			want: "a",
+		},
+		"t is longer than s": {
+			s:    "a",
+			t:    "aa",
+			want: "",
+		},
+		"s is the same as t": {
+			s:    "aa",
+			t:    "aa",
+			want: "aa",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := solution.MinWindow(test.s, test.t)
+			if test.want != got {
+				t.Fatalf("MinWindow(%s, %s): expected = %s, got = %s", test.s, test.t, test.want, got)
+			}
+		})
+	}
+}
