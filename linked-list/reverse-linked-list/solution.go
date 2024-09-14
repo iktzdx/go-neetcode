e193b3d46c895b62b8edf37208fcad78de6daf9b
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-14 23:05:34 +0300

    solution(reverse linked list): add comment to the assignment statement

--- linked-list/reverse-linked-list/solution.go
+++ linked-list/reverse-linked-list/solution.go
@@ -10,6 +10,10 @@ func ReverseList(head *ListNode) *ListNode {
 		return head
 	}
 
+	// https://go.dev/ref/spec#Assignment_statements
+	// The assignment proceeds in two phases:
+	//   - First, the operands on the left and the expressions on the right are all evaluated in the usual order.
+	//   - Second, the assignments are carried out in left-to-right order.
 	head.Next, head, head.Next.Next = nil, ReverseList(head.Next), head
 
 	return head
