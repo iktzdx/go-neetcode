feeadbe1b1b4c9717a19629b48ac6352b94814ef
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-15 15:53:33 +0300

    solution(merge two sorted lists): solve the problem using dummy list node

--- linked-list/merge-two-sorted-lists/solution.go
+++ linked-list/merge-two-sorted-lists/solution.go
@@ -6,17 +6,26 @@ type ListNode struct {
 }
 
 func MergeTwoLists(list1, list2 *ListNode) *ListNode {
-	if list1 == nil {
-		return list2
-	} else if list2 == nil {
-		return list1
+	dummy := &ListNode{}
+	tail := dummy
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			tail.Next = list1
+			list1 = list1.Next
+		} else {
+			tail.Next = list2
+			list2 = list2.Next
+		}
+
+		tail = tail.Next
 	}
 
-	if list1.Val < list2.Val {
-		list1.Next = MergeTwoLists(list1.Next, list2)
-		return list1
+	if list1 != nil {
+		tail.Next = list1
+	} else if list2 != nil {
+		tail.Next = list2
 	}
 
-	list2.Next = MergeTwoLists(list1, list2.Next)
-	return list2
+	return dummy.Next
 }
