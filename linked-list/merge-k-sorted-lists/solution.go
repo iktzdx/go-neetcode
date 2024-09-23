c6c6563d3c50e336cccaa2acde6399b6d9fb9f31
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-23 22:07:58 +0300

    solution(merge k sorted lists): solve the problem by merging only 2 lists at a time - T:O(n log k)

---
+++ linked-list/merge-k-sorted-lists/solution.go
@@ -0,0 +1,56 @@
+package mergeksortedlists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	for len(lists) > 1 {
+		var mergedLists []*ListNode
+
+		for i := 0; i < len(lists); i += 2 {
+			var l1, l2 *ListNode
+
+			l1 = lists[i]
+			if (i + 1) < len(lists) {
+				l2 = lists[i+1]
+			}
+
+			mergedLists = append(mergedLists, mergeLists(l1, l2))
+		}
+
+		lists = mergedLists
+	}
+
+	return lists[0]
+}
+
+func mergeLists(list1, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	tail := dummy
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			tail.Next = list1
+			list1 = list1.Next
+		} else {
+			tail.Next = list2
+			list2 = list2.Next
+		}
+
+		tail = tail.Next
+	}
+
+	if list1 != nil {
+		tail.Next = list1
+	} else if list2 != nil {
+		tail.Next = list2
+	}
+
+	return dummy.Next
+}
