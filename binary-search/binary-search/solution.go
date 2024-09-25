63ac94087fcd7657dfeefdb2b29d09e45dde3420
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-25 21:41:39 +0300

    solution(binary search): fix possible integer overflow

--- binary-search/binary-search/solution.go
+++ binary-search/binary-search/solution.go
@@ -4,7 +4,7 @@ func Search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 
 	for left <= right {
-		mid := (left + right) / 2
+		mid := left + ((right - left) / 2)
 
 		if target > nums[mid] {
 			left = mid + 1
