688fecea9c649ad2e987e3cd29afbbbbd7240a0e
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-25 21:43:27 +0300

    solution(binary search): add an early return to speed up runtime

--- binary-search/binary-search/solution.go
+++ binary-search/binary-search/solution.go
@@ -6,12 +6,14 @@ func Search(nums []int, target int) int {
 	for left <= right {
 		mid := left + ((right - left) / 2)
 
+		if nums[mid] == target {
+			return mid
+		}
+
 		if target > nums[mid] {
 			left = mid + 1
 		} else if target < nums[mid] {
 			right = mid - 1
-		} else {
-			return mid
 		}
 	}
 
