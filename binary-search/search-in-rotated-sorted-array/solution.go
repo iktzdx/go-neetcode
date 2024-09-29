75919fce0fdb6227a268a9f8fe4ab0a5e19bdedd
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-29 20:09:32 +0300

    solution(search in rotated sorted array): solve the problem using binary search - O(log n) time

---
+++ binary-search/search-in-rotated-sorted-array/solution.go
@@ -0,0 +1,29 @@
+package searchinrotatedsortedarray
+
+func Search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if target == nums[mid] {
+			return mid
+		}
+
+		if nums[mid] >= nums[left] {
+			if target > nums[mid] || target < nums[left] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if target < nums[mid] || target > nums[right] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
