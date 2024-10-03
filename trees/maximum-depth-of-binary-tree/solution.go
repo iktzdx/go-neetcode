5ef4bfd92558e8e5e04fddeae0ba12fc7c938ee9
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-10-03 21:07:01 +0300

    solution(maximum depth of binary tree): solve the problem using BFS - O(n) time, O(n) memory

--- trees/maximum-depth-of-binary-tree/solution.go
+++ trees/maximum-depth-of-binary-tree/solution.go
@@ -7,5 +7,23 @@ func MaxDepth(root *trees.TreeNode) int {
 		return 0
 	}
 
-	return 1 + max(MaxDepth(root.Left), MaxDepth(root.Right))
+	level := 0
+	queue := []*trees.TreeNode{root}
+
+	for len(queue) > 0 {
+		for range len(queue) {
+			var node *trees.TreeNode
+			node, queue = queue[0], queue[1:]
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		level++
+	}
+
+	return level
 }
