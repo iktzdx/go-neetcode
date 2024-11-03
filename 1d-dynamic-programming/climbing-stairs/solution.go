db51ee211a49a7417b08aa071c60c2467d56ee25
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-11-03 23:59:03 +0300

    solution(climbing stairs): solve the problem using dynamic drogramming (bottom-up)

--- 1d-dynamic-programming/climbing-stairs/solution.go
+++ 1d-dynamic-programming/climbing-stairs/solution.go
@@ -1,29 +1,11 @@
 package climbingstairs
 
-type dfsFunc func(int) int
-
 func ClimbStairs(n int) int {
-	var dfs dfsFunc
+	one, two := 1, 1
 
-	dp := make(map[int]int)
-
-	dfs = func(i int) int {
-		if i == n {
-			return 1
-		}
-
-		if i > n {
-			return 0
-		}
-
-		if val, ok := dp[i]; ok {
-			return val
-		}
-
-		dp[i] = dfs(i+1) + dfs(i+2)
-
-		return dp[i]
+	for range n - 1 {
+		one, two = one+two, one
 	}
 
-	return dfs(0)
+	return one
 }
