26b19ffc9c4f262116911196e50775557d881716
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-11-03 23:48:40 +0300

    solution(climbing stairs): solve the problem using recurisve dfs with memoization - O(n) time

--- 1d-dynamic-programming/climbing-stairs/solution.go
+++ 1d-dynamic-programming/climbing-stairs/solution.go
@@ -5,16 +5,24 @@ type dfsFunc func(int) int
 func ClimbStairs(n int) int {
 	var dfs dfsFunc
 
+	dp := make(map[int]int)
+
 	dfs = func(i int) int {
 		if i == n {
 			return 1
 		}
 
-		if i < n {
-			return dfs(i+1) + dfs(i+2)
+		if i > n {
+			return 0
 		}
 
-		return 0
+		if val, ok := dp[i]; ok {
+			return val
+		}
+
+		dp[i] = dfs(i+1) + dfs(i+2)
+
+		return dp[i]
 	}
 
 	return dfs(0)
