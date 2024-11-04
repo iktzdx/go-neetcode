50221eba2008607aab101ba99aff09a4c109b2f5
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-11-04 23:59:29 +0300

    test(design twitter): improve test coverage

--- heap/design-twitter/solution_test.go
+++ heap/design-twitter/solution_test.go
@@ -2,6 +2,7 @@ package designtwitter_test
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -46,4 +47,23 @@ func Test_Twitter(t *testing.T) {
 	// since user 1 is no longer following user 2.
 	f1 = tw.GetNewsFeed(1)
 	require.True(t, reflect.DeepEqual([]int{5}, f1), "get user 1's news feed after unfollow")
+
+	tweetsID := []int{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+
+	// User 3 posts a new tweet.
+	for _, id := range tweetsID {
+		tw.PostTweet(3, id)
+	}
+
+	// Should return tweets ID in descending order.
+	sort.Slice(tweetsID, func(i, j int) bool {
+		return tweetsID[i] > tweetsID[j]
+	})
+
+	// User 1 follows user 3.
+	tw.Follow(1, 3)
+
+	// User 1's news feed should return a list with 10 recent tweet IDs.
+	f1 = tw.GetNewsFeed(1)
+	require.True(t, reflect.DeepEqual(tweetsID[:10], f1), "get 10 recent tweet IDs from user 1's news feed")
 }
